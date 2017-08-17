0fa678077bed863159c76f75a40d627ca06041d7
oglopss <oglopss@gmail.com>
2017-08-16 17:37:43 -0700

    add automake 1.8, prev error restartable system calls

--- .travis.sh
+++ .travis.sh
@@ -26,12 +26,12 @@ cd advancedtomato/release/src-rt
 
 
 
-# cd ~
-# wget http://ftp.gnu.org/gnu/automake/automake-1.8.tar.gz
-# tar xf automake-1.8.tar.gz
-# cd automake-1.8
-# sh configure #--prefix /usr
-# sudo make install
+cd ~
+wget http://ftp.gnu.org/gnu/automake/automake-1.8.tar.gz
+tar xf automake-1.8.tar.gz
+cd automake-1.8
+sh configure #--prefix /usr
+sudo make install
 
 # cd ~
 # wget http://ftp.gnu.org/gnu/automake/automake-1.10.tar.gz
