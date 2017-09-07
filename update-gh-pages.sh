08e93baf167215f7c42a9157344082f4420054b7
oglopss <oglopss@gmail.com>
2017-09-07 08:14:19 -0700

    test if fw does not exist

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -115,7 +115,15 @@ push_changes()
 
   cd $HOME/gh-pages-$TT_BUILD/download
 
-  cp -Rf $image/$fw .
+  if [ -f $image/$fw ]; then
+    cp -Rf $image/$fw .
+  else
+    echo "$fw not found!"
+    fw=""
+  fi
+
+
+  
   # cd $HOME/gh-pages-$TT_BUILD/download
 
   #add, commit and push files
