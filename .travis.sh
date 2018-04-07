afb5937a813958e650183a84ee0eb4503190dfb2
oglopss <oglopss@gmail.com>
2018-04-07 01:02:37 -0700

    cross build uuid /usr

--- .travis.sh
+++ .travis.sh
@@ -183,7 +183,7 @@ wget https://downloads.sourceforge.net/project/libuuid/libuuid-1.0.3.tar.gz
 tar xf libuuid-1.0.3.tar.gz
 cd libuuid-1.0.3
 # sh configure --prefix /usr
-CC=mipsel-uclibc-gcc CXX=mipsel-uclibc-g++ AR=mipsel-uclibc-ar RANLIB=/opt/brcm-arm/bin/mipsel-uclibc-ranlib ./configure --host=mipsel-uclibc-linux 
+CC=mipsel-uclibc-gcc CXX=mipsel-uclibc-g++ AR=mipsel-uclibc-ar RANLIB=/opt/brcm-arm/bin/mipsel-uclibc-ranlib ./configure --host=mipsel-uclibc-linux --prefix=/usr
 # --prefix=$HOME/uuid-install
 make
 sudo  make install
