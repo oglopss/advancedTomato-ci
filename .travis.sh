bab5dd8cb7eb59219b767d9cee7aeffbc40d73f8
oglopss <oglopss@gmail.com>
2018-04-08 10:18:48 -0700

    show full make output for testing

--- .travis.sh
+++ .travis.sh
@@ -307,7 +307,8 @@ build_tomato()
     # time make V1=RT-N5x-CN- V2=-140 r2z  > ~/advancedTomato.txt
 
     # make V1=RT-N5x-CN- V2=-140 r2z &
-    make V1=RT-N5x-CN- V2=-140 $TT_BUILD > /dev/null &
+    #make V1=RT-N5x-CN- V2=-140 $TT_BUILD > /dev/null &
+    make V1=RT-N5x-CN- V2=-140 $TT_BUILD 
 
 
     local build_pid=$!
